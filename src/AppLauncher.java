414386f4932f270fcd43c3954ce3a9246c247f71
Allie Costner <alliecostner3@gmail.com>
2024-12-05 15:03:04 -0500

    update launcher

--- src/AppLauncher.java
+++ src/AppLauncher.java
@@ -2,13 +2,16 @@ import javax.swing.*;
 
 public class AppLauncher {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
+        SwingUtilities.invokeLater(new Runnable(){
             @Override
-            public void run() {
-                //display gui
-               new WeatherAppGui().setVisible(true);
+            public void run(){
+                //display the gui
+                new WeatherAppGui().setVisible(true);
 
                 //System.out.println(WeatherApp.getLocationData("Gastonia"));
+
+               //System.out.println(WeatherApp.getCurrentTime());
+
             }
         });
     }
